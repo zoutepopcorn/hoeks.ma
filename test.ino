6b20c37e28bd4988ab4d63c80299d999b7b6f72d
zoutepopcorn <zoutepopcorn@hotmail.com>
2016-01-08 11:45:11 +0100

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,208 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Matthijs Kooijman and Thomas Telkamp 
+ * HLC (Hoeks.ma Location) .ino file edited by Johan Hoeksma                 
+ * You will need a esp8266
+ * 
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello, world!", that
+ * will be processed by The Things Network server.
+ *
+ * Change DEVADDR to a unique address!
+ *
+ * Do not forget to define the radio type correctly in config.h, default is:
+ *   #define CFG_sx1272_radio 1
+ * for SX1272 and RFM92, but change to:
+ *   #define CFG_sx1276_radio 1
+ * for SX1276 and RFM95.
+ *
+ *
+ *******************************************************************************/
+#include "ESP8266WiFi.h"
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+// LoRaWAN Application identifier (AppEUI)
+// Not used in this example
+static const u1_t APPEUI[8]  = { 0x02, 0x00, 0x00, 0x00, 0x00, 0xEE, 0xFF, 0xC0 };
+
+// LoRaWAN DevEUI, unique device ID (LSBF)
+// Not used in this example
+static const u1_t DEVEUI[8]  = { 0x42, 0x42, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
+
+// LoRaWAN NwkSKey, network session key 
+// Use this key for The Things Network
+static const u1_t DEVKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+
+// LoRaWAN AppSKey, application session key
+// Use this key to get your data decrypted by The Things Network
+static const u1_t ARTKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x020106AA; // <-- Change this address for every node!
+
+//////////////////////////////////////////////////
+// APPLICATION CALLBACKS
+//////////////////////////////////////////////////
+
+// provide application router ID (8 bytes, LSBF)
+void os_getArtEui (u1_t* buf) {
+    memcpy(buf, APPEUI, 8);
+}
+
+// provide device ID (8 bytes, LSBF)
+void os_getDevEui (u1_t* buf) {
+    memcpy(buf, DEVEUI, 8);
+}
+
+// provide device key (16 bytes)
+void os_getDevKey (u1_t* buf) {
+    memcpy(buf, DEVKEY, 16);
+}
+
+uint8_t mydata[] = "No wifi spots found";
+uint8_t macs[28];  // Array to store the 4 mac for
+uint8_t rssi[4];   // Array to store the 4 rssi in char
+char buffer[64];
+static osjob_t sendjob;
+int datarate=0;
+
+// Pin mapping
+lmic_pinmap pins = {
+  .nss = D8,
+  .rxtx = D9, // Not connected on RFM92/RFM95
+  .rst = D4,  // Needed on RFM92/RFM95
+  .dio = {D1, D2, D3},
+};
+
+boolean hasLocation() {
+  int n = WiFi.scanNetworks(); // number of acces points found
+  
+  if (n == 0) {
+    Serial.println("Nope. No spots, are you lost or what :(");
+    return false;
+  } else {
+    Serial.print(n);
+    Serial.println("Yup, found some Wifi spots :D");
+    for (int i = 0; i < n; ++i) {
+      // Send max 4 macs + rssi
+      unsigned char te = (char)WiFi.BSSID(i)[0]; // TODO save it in rssi array
+      Serial.println(te);
+      // TODO, scan more or make it random to save data
+      if(i < 4) {
+        Serial.print(WiFi.SSID(i));
+        Serial.println();
+        Serial.print(WiFi.BSSID(i)[0]);
+        Serial.print(":");
+        Serial.print(WiFi.BSSID(i)[1]); 
+        Serial.print(":");
+        Serial.print(WiFi.BSSID(i)[2]);
+        Serial.print(":");
+        Serial.print(WiFi.BSSID(i)[3]);
+        Serial.print(":");
+        Serial.print(WiFi.BSSID(i)[4]);
+        Serial.print(":");
+        Serial.print(WiFi.BSSID(i)[5]);
+        rssi[i] = (uint8_t)((-1) * WiFi.RSSI(i)); // convert RSSI it to a positive char
+        for(int c = 0; c < 5; c++) {
+            macs[(i * 6) + c] = WiFi.BSSID(i)[c];
+        }
+      }
+      delay(10);
+    }
+  }
+  Serial.println("");
+  return true;
+}
+
+void onEvent (ev_t ev) {
+    //debug_event(ev);
+    switch(ev) {
+      // scheduled data sent (optionally data received)
+      // note: this includes the receive window!
+      case EV_TXCOMPLETE:
+          Serial.print("Event EV_TXCOMPLETE, time: ");
+          Serial.println(millis() / 1000);
+          if(LMIC.dataLen) { // data received in rx slot after tx
+              //debug_buf(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+              Serial.println("Data Received!");
+          }
+          break;
+       default:
+          break;
+    }
+}
+
+void do_send(osjob_t* j){
+     yield();
+      // LoRa Stuff
+      Serial.print("Time: ");
+      Serial.println(millis() / 1000);
+      // Show TX channel (channel numbers are local to LMIC)
+      Serial.print("Send, txCnhl: ");
+      Serial.println(LMIC.txChnl);
+      Serial.print("TX dBm: ");
+      Serial.println(LMIC.txpow);
+      Serial.print("Opmode check: ");
+      // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & (1 << 7)) {
+      Serial.println("OP_TXRXPEND, not sending");
+    } else {
+      Serial.println("ok");
+      // Prepare upstream data transmission at the next possible time.
+      datarate++;
+      if (datarate>7) { datarate=0; }
+      //LMIC_setDrTxpow((dr_t)datarate,17);
+      delay(10);
+      if(hasLocation()) {
+        LMIC_setTxData2(1, macs, sizeof(macs)-1, 0);
+      } else {
+        LMIC_setTxData2(1, (unsigned char*)mydata, sizeof(mydata)-1, 0);
+      }
+    }
+    // Schedule a timed job to run at the given timestamp (absolute system time)
+    os_setTimedCallback(j, os_getTime()+sec2osticks(30), do_send);
+         
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting");
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+  // Set static session parameters. Instead of dynamically establishing a session 
+  // by joining the network, precomputed session parameters are be provided.
+  LMIC_setSession (0x1, DEVADDR, (uint8_t*)DEVKEY, (uint8_t*)ARTKEY);
+  // Disable data rate adaptation
+  LMIC_setAdrMode(0);
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
+  // Disable beacon tracking
+  LMIC_disableTracking ();
+  // Stop listening for downstream data (periodical reception)
+  LMIC_stopPingable();
+  // Set data rate and transmit power
+  LMIC_setDrTxpow(DR_SF7,14);
+  //
+  LMIC_setupBand (BAND_MILLI, 14, 10);
+  LMIC_setupBand (BAND_CENTI, 14, 10);
+  // 
+  delay(100);
+  // 
+  Serial.flush();
+}
+
+void loop() {
+  do_send(&sendjob);
+  while(1) {
+    os_runloop_once();
+    delay(10);
+  }
+}
