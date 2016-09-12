27b5d658cbf0ac7b909cfb204c01ab3cba59209c
zoutepopcorn <zoutepopcorn@hotmail.com>
2016-09-12 16:08:12 +0200

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2015 Matthijs Kooijman and Thomas Telkamp 
+ * 
  * HLC (Hoeks.ma Location) .ino file edited by Johan Hoeksma                 
  * You will need a esp8266
  * 
@@ -26,93 +27,103 @@
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
+#include <Arduino.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 
-// LoRaWAN Application identifier (AppEUI)
-// Not used in this example
-static const u1_t APPEUI[8]  = { 0x02, 0x00, 0x00, 0x00, 0x00, 0xEE, 0xFF, 0xC0 };
+#define USE_SERIAL Serial
 
-// LoRaWAN DevEUI, unique device ID (LSBF)
-// Not used in this example
-static const u1_t DEVEUI[8]  = { 0x42, 0x42, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
+// use ttn dashboard to get this keys
+static const PROGMEM u1_t NWKSKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u1_t PROGMEM APPSKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u4_t DEVADDR = 0x00000000; // <-- Change this address for every node!
 
-// LoRaWAN NwkSKey, network session key 
-// Use this key for The Things Network
-static const u1_t DEVKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-
-// LoRaWAN AppSKey, application session key
-// Use this key to get your data decrypted by The Things Network
-static const u1_t ARTKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-
-// LoRaWAN end-device address (DevAddr)
-static const u4_t DEVADDR = 0x020106AA; // <-- Change this address for every node!
-
-//////////////////////////////////////////////////
-// APPLICATION CALLBACKS
-//////////////////////////////////////////////////
-
-// provide application router ID (8 bytes, LSBF)
-void os_getArtEui (u1_t* buf) {
-    memcpy(buf, APPEUI, 8);
-}
-
-// provide device ID (8 bytes, LSBF)
-void os_getDevEui (u1_t* buf) {
-    memcpy(buf, DEVEUI, 8);
-}
-
-// provide device key (16 bytes)
-void os_getDevKey (u1_t* buf) {
-    memcpy(buf, DEVKEY, 16);
-}
-
-uint8_t mydata[] = "No wifi spots found";
-uint8_t macs[28];  // Array to store the 4 mac for
-uint8_t rssi[4];   // Array to store the 4 rssi in char
+// WiFi vars
+static const int NR_MACS = 3;
+static uint8_t mydata[] = "test";
+uint8_t macs[NR_MACS * 7];    // Array to store the 4 mac for  // (nrMacs * 7) + nrMacs
 char buffer[64];
-static osjob_t sendjob;
-int datarate=0;
 
-// Pin mapping
-lmic_pinmap pins = {
-  .nss = D8,
-  .rxtx = D9, // Not connected on RFM92/RFM95
-  .rst = D4,  // Needed on RFM92/RFM95
-  .dio = {D1, D2, D3},
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+ESP8266WiFiMulti WiFiMulti;
+
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+// Pin mapping ESP8266   D8 -> Slave select
+const lmic_pinmap lmic_pins = {
+    .nss = D8,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = LMIC_UNUSED_PIN,
+    .dio = {LMIC_UNUSED_PIN, LMIC_UNUSED_PIN, LMIC_UNUSED_PIN},
 };
+
+void ota() {
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    Serial.println("ota");
+    WiFiMulti.addAP("ssid", "12345678");
+    for(uint8_t t = 5; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+    
+    Serial.println("setup complete");
+    delay(5000);
+    if(WiFiMulti.run() != WL_CONNECTED)  {
+        Serial.println("No wifi update shutting down wifi");
+    } else {
+        Serial.println("updating");
+        t_httpUpdate_return ret = ESPhttpUpdate.update("http://server.nl/ota.bin");
+
+        switch(ret) {
+            case HTTP_UPDATE_FAILED:
+                USE_SERIAL.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+                break;
+
+            case HTTP_UPDATE_NO_UPDATES:
+                USE_SERIAL.println("HTTP_UPDATE_NO_UPDATES");
+                break;
+
+            case HTTP_UPDATE_OK:
+                USE_SERIAL.println("HTTP_UPDATE_OK");
+                break;
+        }
+    }  
+}
 
 boolean hasLocation() {
   int n = WiFi.scanNetworks(); // number of acces points found
-  
   if (n == 0) {
     Serial.println("Nope. No spots, are you lost or what :(");
     return false;
   } else {
+    Serial.print("I found ");
     Serial.print(n);
-    Serial.println("Yup, found some Wifi spots :D");
+    Serial.println(" spots!");
+    if(n > NR_MACS) {
+      n = NR_MACS;
+    }
+    // TODO: send only strongest RSSI??
     for (int i = 0; i < n; ++i) {
-      // Send max 4 macs + rssi
-      unsigned char te = (char)WiFi.BSSID(i)[0]; // TODO save it in rssi array
-      Serial.println(te);
-      // TODO, scan more or make it random to save data
-      if(i < 4) {
-        Serial.print(WiFi.SSID(i));
         Serial.println();
-        Serial.print(WiFi.BSSID(i)[0]);
-        Serial.print(":");
-        Serial.print(WiFi.BSSID(i)[1]); 
-        Serial.print(":");
-        Serial.print(WiFi.BSSID(i)[2]);
-        Serial.print(":");
-        Serial.print(WiFi.BSSID(i)[3]);
-        Serial.print(":");
-        Serial.print(WiFi.BSSID(i)[4]);
-        Serial.print(":");
-        Serial.print(WiFi.BSSID(i)[5]);
-        rssi[i] = (uint8_t)((-1) * WiFi.RSSI(i)); // convert RSSI it to a positive char
-        for(int c = 0; c < 5; c++) {
-            macs[(i * 6) + c] = WiFi.BSSID(i)[c];
+        Serial.print("rssi");
+        Serial.println(WiFi.RSSI(i));
+        for(int c = 0; c < 6; c++) {
+            macs[(i * 7) + c] = (uint8_t)WiFi.BSSID(i)[c];
+            Serial.print(WiFi.BSSID(i)[c]);
+            Serial.print(":");
         }
-      }
+        Serial.println();
+        macs[(i*7)+6] =  (uint8_t)((-1) * WiFi.RSSI(i));
       delay(10);
     }
   }
@@ -120,89 +131,130 @@ boolean hasLocation() {
   return true;
 }
 
-void onEvent (ev_t ev) {
-    //debug_event(ev);
-    switch(ev) {
-      // scheduled data sent (optionally data received)
-      // note: this includes the receive window!
-      case EV_TXCOMPLETE:
-          Serial.print("Event EV_TXCOMPLETE, time: ");
-          Serial.println(millis() / 1000);
-          if(LMIC.dataLen) { // data received in rx slot after tx
-              //debug_buf(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
-              Serial.println("Data Received!");
-          }
-          break;
-       default:
-          break;
+void do_send(osjob_t* j){
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        if(hasLocation()) {
+          LMIC_setTxData2(1, macs, sizeof(macs), 0);
+        } else {
+          LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
+        }
+        Serial.println(F("Packet queued"));
     }
+    // Next TX is scheduled after TX_COMPLETE event.
 }
 
-void do_send(osjob_t* j){
-     yield();
-      // LoRa Stuff
-      Serial.print("Time: ");
-      Serial.println(millis() / 1000);
-      // Show TX channel (channel numbers are local to LMIC)
-      Serial.print("Send, txCnhl: ");
-      Serial.println(LMIC.txChnl);
-      Serial.print("TX dBm: ");
-      Serial.println(LMIC.txpow);
-      Serial.print("Opmode check: ");
-      // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & (1 << 7)) {
-      Serial.println("OP_TXRXPEND, not sending");
-    } else {
-      Serial.println("ok");
-      // Prepare upstream data transmission at the next possible time.
-      datarate++;
-      if (datarate>7) { datarate=0; }
-      //LMIC_setDrTxpow((dr_t)datarate,17);
-      delay(10);
-      if(hasLocation()) {
-        LMIC_setTxData2(1, macs, sizeof(macs)-1, 0);
-      } else {
-        LMIC_setTxData2(1, (unsigned char*)mydata, sizeof(mydata)-1, 0);
-      }
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.println(F("Received "));
+              Serial.println(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
     }
-    // Schedule a timed job to run at the given timestamp (absolute system time)
-    os_setTimedCallback(j, os_getTime()+sec2osticks(30), do_send);
-         
 }
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting");
-  // LMIC init
-  os_init();
-  // Reset the MAC state. Session and pending data transfers will be discarded.
-  LMIC_reset();
-  // Set static session parameters. Instead of dynamically establishing a session 
-  // by joining the network, precomputed session parameters are be provided.
-  LMIC_setSession (0x1, DEVADDR, (uint8_t*)DEVKEY, (uint8_t*)ARTKEY);
-  // Disable data rate adaptation
-  LMIC_setAdrMode(0);
-  // Disable link check validation
-  LMIC_setLinkCheckMode(0);
-  // Disable beacon tracking
-  LMIC_disableTracking ();
-  // Stop listening for downstream data (periodical reception)
-  LMIC_stopPingable();
-  // Set data rate and transmit power
-  LMIC_setDrTxpow(DR_SF7,14);
-  //
-  LMIC_setupBand (BAND_MILLI, 14, 10);
-  LMIC_setupBand (BAND_CENTI, 14, 10);
-  // 
-  delay(100);
-  // 
-  Serial.flush();
+    Serial.begin(115200);
+    Serial.println(F("Starting"));
+    WiFi.mode(WIFI_STA);
+    // check if it needs OTA update..
+    ota();
+    WiFi.mode(WIFI_OFF);
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+    #ifdef PROGMEM
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    #if defined(CFG_eu868)
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868100000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    #elif defined(CFG_us915)
+    LMIC_selectSubBand(1);
+    #endif
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+    // Start job
+    do_send(&sendjob);
 }
 
 void loop() {
-  do_send(&sendjob);
-  while(1) {
     os_runloop_once();
-    delay(10);
-  }
 }
